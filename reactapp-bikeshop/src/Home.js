704b4bd236b2fab1d4fa86baf8b110cce4af2552
tranjg <tranjg@mail.uc.edu>
2020-12-18 17:10:36 -0500

    removed unused imports;

--- reactapp-bikeshop/src/Home.js
+++ reactapp-bikeshop/src/Home.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid';
 import Paper from "@material-ui/core/Paper";
-import Image from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
 import Orders from "./Orders";
 
 const useStyles = makeStyles((theme) => ({
