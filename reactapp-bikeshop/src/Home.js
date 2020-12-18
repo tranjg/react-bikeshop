5e81de4f249b4e3c8f3a524b0c15deca2a326bf7
tranjg <tranjg@mail.uc.edu>
2020-12-18 17:09:33 -0500

    added Order component to Home page;

---
+++ reactapp-bikeshop/src/Home.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import Paper from "@material-ui/core/Paper";
+import Image from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Orders from "./Orders";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    container: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+}));
+
+export default function Home() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>
+                        <Orders />
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+
+    )
+}
+
+
