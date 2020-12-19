dbfec537619cbcd72f3f27025183d1b1ccf43115
tranjg <tranjg@mail.uc.edu>
2020-12-19 00:47:45 -0500

    created data table using material-table;

---
+++ reactapp-bikeshop/src/InventoryTable.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import MaterialTable from "material-table";
+import { forwardRef } from 'react';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
+
+export default function InventoryTable() {
+    return(
+        <MaterialTable
+            icons={tableIcons}
+            title="Current Inventory"
+            columns={[
+                { title: 'ID', field: 'id'},
+                { title: 'Item', field: 'item' },
+                { title: 'Quantity', field: 'quantity'},
+            ]}
+            data={[
+                { id: 1, item: 'Gear', quantity: 34 },
+                { id: 2, item: 'Nut', quantity: 102 },
+                { id: 3, item: 'WD-40', quantity: 14 },
+                { id: 4, item: 'BMX Wheel', quantity: 27 },
+                { id: 5, item: 'BMX Frame', quantity: 38 },
+                { id: 6, item: 'Mountain Frame', quantity: 22 },
+            ]}
+            options={{
+                search: true
+            }}
+        />
+    )
+}
