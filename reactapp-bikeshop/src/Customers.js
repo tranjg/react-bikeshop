2df5155b82baa914ae3f8e50d4432ddff3a46289
tranjg <tranjg@mail.uc.edu>
2020-12-19 01:12:59 -0500

    added CustomerTable component;

---
+++ reactapp-bikeshop/src/Customers.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from '@material-ui/core/styles';
+import Orders from "./Orders";
+import CustomerTable from "./CustomerTable";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    container: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+}));
+
+export default function Customers() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Grid container spacing={3}>
+                {/* Customers */}
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>
+                    <CustomerTable/>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+
+    )
+}
+
+
