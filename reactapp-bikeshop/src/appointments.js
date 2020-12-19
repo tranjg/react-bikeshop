c1b3949fe39222df71e7608683c4571b7a3ee158
tranjg <tranjg@mail.uc.edu>
2020-12-18 23:22:40 -0500

    created appointment data;

---
+++ reactapp-bikeshop/src/appointments.js
@@ -0,0 +1,18 @@
+export const appointments = [
+    {
+        title: 'John Doe',
+        startDate: new Date(2020, 11, 19, 9, 35),
+        endDate: new Date(2020, 11, 19, 11, 30),
+        id: 0,
+    }, {
+        title: 'Steve Jobs',
+        startDate: new Date(2020, 11, 20, 10, 20),
+        endDate: new Date(2020, 11, 20, 12, 20),
+        id: 1,
+    }, {
+        title: 'Marc Jacobs',
+        startDate: new Date(2020, 11, 22, 9, 35),
+        endDate: new Date(2020, 11, 22, 11, 30),
+        id: 2,
+    },
+];
